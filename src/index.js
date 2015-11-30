a115d137cefc958f605233b8c6fff9fd2433837a
Remon Oldenbeuving <remon@hackerone.com>
2015-11-30 14:45:01 +0100

    Temporarily disable variable fixing
    This feature breaks inside of for-loops. I should investigate what's going on.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var coffeeScript = require('coffee-react')
 var jsCodeShift = require('jscodeshift')
 var requireToImportTransform = require('./transforms/require-to-import')
 var moduleExportsToExportTransform = require('./transforms/module-exports-to-export')
-var implicitVariableDeclarationTransform = require('./transforms/implicit-variable-declaration')
+// var implicitVariableDeclarationTransform = require('./transforms/implicit-variable-declaration')
 var classMethodTransform = require('./transforms/class-method')
 var fatArrowTransform = require('./transforms/fat-arrow')
 var jsxTransform = require('./transforms/jsx')
@@ -33,9 +33,9 @@ function espresso (content, opts) {
     newContent = fatArrowTransform({ source: newContent }, api)
   }
 
-  if (opts.implicitVariableDeclaration) {
-    newContent = implicitVariableDeclarationTransform({ source: newContent }, api)
-  }
+  // if (opts.implicitVariableDeclaration) {
+  //   newContent = implicitVariableDeclarationTransform({ source: newContent }, api)
+  // }
 
   if (opts.jsx) {
     newContent = jsxTransform({source: newContent}, api)
