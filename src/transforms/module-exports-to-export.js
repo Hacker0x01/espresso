1186765d1478e62601c43cceae2e10e06e3312e6
Remon Oldenbeuving <remon@hackerone.com>
2015-12-04 14:39:28 +0100

    Clean up var declarations for imported modules

--- src/transforms/module-exports-to-export.js
+++ src/transforms/module-exports-to-export.js
@@ -19,5 +19,30 @@ function moduleExportsToExport(file, api) {
     .find(j.ExpressionStatement, MODULE_EXPORTS)
     .replaceWith(p => j.exportDeclaration(true, p.node.expression.right));
 
+
+  root
+    .find(j.ImportDeclaration)
+    .filter(importPath => importPath.value.specifiers.length === 1)
+    .forEach(importPath => {
+      j(importPath.scope.path)
+        .find(j.VariableDeclarator, {
+          id: {
+            type: "Identifier",
+            name: importPath.value.specifiers[0].local.name
+          }
+        })
+        .forEach(declaratorPath => {
+          var declarationPath = declaratorPath.parent;
+          var declarations = declarationPath.value.declarations;
+          var indexOfDeclarator = declarations.indexOf(declaratorPath.value);
+
+          declarations.splice(indexOfDeclarator, 1);
+
+          if (declarations.length === 0) {
+            j(declarationPath).remove();
+          }
+        });
+    });
+
   return root.toSource();
 }
