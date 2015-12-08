22f4a48d624cf679ed39cd56353e1df0e23886b0
Remon Oldenbeuving <remon@hackerone.com>
2015-12-08 11:34:33 +0100

    Rewrote template transform to allow for more cases
    Newly supported cases:
    ```
    "/#{expression}/"
    
    `/${expression}/team_members`
    ```

--- src/transforms/string-interpolation.js
+++ src/transforms/string-interpolation.js
@@ -1,51 +1,56 @@
-const transformExpressionsIntoTemplates = (root, j) => {
-  const simpleExpressionsChanged = root
-    .find(j.BinaryExpression, { operator: "+", left: { type: "Literal" } })
-    .filter(exp => typeof exp.value.left.value === "string")
-    .replaceWith(exp =>
-      j.templateLiteral(
-        [
-          j.templateElement({
-            cooked: exp.value.left.value,
-            raw: exp.value.left.rawValue
-          }, false)
-        ],
-        [
-          exp.value.right
-        ]
-      )
-    )
-    .size() > 0;
+const transformExpressionsIntoTemplates = (root, j) =>
+  root
+    .find(j.BinaryExpression, { operator: "+" })
+    .filter(exp => {
+      var left = exp.value.left;
+      var right = exp.value.right;
 
-  const binaryExpressionsWithTemplatesChanged = root
-    .find(j.BinaryExpression, { operator: "+", left: { type: "TemplateLiteral" } })
+      var stringRight = right.type === "Literal" && typeof right.value === "string";
+      var stringLeft = left.type === "Literal" && typeof left.value === "string";
+      var binaryExpressionLeft = left.type === "BinaryExpression";
+
+      var involvesStrings = stringLeft || (stringRight && !binaryExpressionLeft)
+      var involvesTemplates = exp.value.left.type === "TemplateLiteral" ||
+        exp.value.right.type === "TemplateLiteral";
+
+      return involvesStrings || involvesTemplates;
+    })
     .replaceWith(exp => {
-      var templateLiteral = exp.value.left;
+      var quasisFromLiteral = (literal) =>
+        j.templateElement({ cooked: literal.value, raw: literal.rawValue }, false);
+      var left = exp.value.left;
+      var right = exp.value.right;
+      var stringRight = right.type === "Literal" && typeof right.value === "string";
+      var stringLeft = left.type === "Literal" && typeof left.value === "string";
 
-      if (exp.value.right.type === "Literal") {
-        templateLiteral.quasis.push(
-          j.templateElement({
-            cooked: exp.value.right.value,
-            raw: exp.value.right.rawValue
-          }, false)
-        );
+      var quasis = [];
+      var expressions = [];
+
+      if (stringLeft) {
+        quasis.push(quasisFromLiteral(left));
+      } else if (left.type === "TemplateLiteral") {
+        quasis.push.apply(quasis, left.quasis);
+        expressions.push.apply(expressions, left.expressions);
       } else {
-        templateLiteral.expressions.push(exp.value.right);
-
-        if (templateLiteral.expressions.length > templateLiteral.quasis.length) {
-          templateLiteral.quasis.push(j.templateElement({
-            cooked: "",
-            raw: ""
-          }, false));
-        }
+        quasis.push(j.templateElement({ cooked: '', raw: '' }, false));
+        expressions.push(left);
       }
 
-      return templateLiteral;
+      if (stringRight) {
+        quasis.push(quasisFromLiteral(right));
+      } else if (right.type === "TemplateLiteral") {
+        quasis.push.apply(quasis, right.quasis);
+        expressions.push.apply(expressions, right.expressions);
+      } else {
+        if (quasis.length === 0)
+          quasis.push(j.templateElement({ cooked: '', raw: '' }, false));
+
+        expressions.push(right);
+      }
+
+      return j.templateLiteral(quasis, expressions);
     })
     .size() > 0;
-
-  return simpleExpressionsChanged || binaryExpressionsWithTemplatesChanged;
-};
 
 module.exports = function(file, api) {
   const j = api.jscodeshift;
